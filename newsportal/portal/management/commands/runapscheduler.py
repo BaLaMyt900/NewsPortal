1e4be489dd3ce528fde9c4f46000a9f6e43e665b
BaLaMyt900 <57708651+BaLaMyt900@users.noreply.github.com>
2023-06-01 18:30:41 +0400

    Добавлена еженедельная рассылка подписчикам в понедельник.

--- newsportal/portal/management/commands/runapscheduler.py
+++ newsportal/portal/management/commands/runapscheduler.py
@@ -1,21 +1,40 @@
 import logging
+import datetime
 from django.conf import settings
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
-from portal.models import Author
+from portal.models import Author, Subscribers, Post, Category
 
 logger = logging.getLogger(__name__)
 
 
-# # наша задача по выводу текста на экран
-# def my_job():
-#     #  Your job processing logic here...
-#     print('hello from job')
+def previous_week_sending():  # Функция сбора и отправки постов за прошлую неделю подписчикам
+    for cat in Category.objects.all():
+        subs = [_.user for _ in Subscribers.objects.filter(category=cat)]  # Список пользователей
+        week = int(datetime.datetime.today().strftime('%V')) - 1  # Номер прошлой недели. Для проверки можно убрать '- 1'
+        posts = Post.objects.filter(post_time__week=week, categories=cat)  # Посты выбранной категории
+        if posts:
+            for user in subs:
+                if user.email:  # Если у подписчика есть почта
+                    msg = render_to_string('account/email/new_week_mail.html', context={'user': user.username,
+                                                                                        'posts': posts,
+                                                                                        'cat': cat.name})
+                    message = EmailMessage(  # Сборка сообщения
+                        'Новая выгрузка из Вашей любимой категории: {cat.name}',
+                        msg,
+                        from_email=settings.DEFAULT_FROM_EMAIL,
+                        to=[user.email]
+                    )
+                    message.content_subtype = 'html'  # Указание, что это html сообщение
+                    message.send()  #  Отправка
 
-def drop_numbers_of_posts():
+
+def drop_numbers_of_posts():  # Функция сброса колличества поство в день. Запуск в полночь
     for author in Author.objects.all():
         author.update_numbers_of_posts()
 
@@ -33,23 +52,20 @@ class Command(BaseCommand):
         scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
-        # # добавляем работу нашему задачнику
-        # scheduler.add_job(
-        #     my_job,
-        #     trigger=CronTrigger(second="*/10"),
-        #     # То же, что и интервал, но задача тригера таким образом более понятна django
-        #     id="my_job",  # уникальный айди
-        #     max_instances=1,
-        #     replace_existing=True,
-        # )
-        # logger.info("Added job 'my_job'.")
-
         scheduler.add_job(
             drop_numbers_of_posts,
             trigger=CronTrigger(hour='00', minute='00', second='00'),
             id='drop_numbers_of_posts',
             max_instances=1,
             replace_existing=True,
+        )
+
+        scheduler.add_job(
+            previous_week_sending,
+            trigger=CronTrigger(day_of_week='mon'),
+            id='previous_week_sending',
+            max_instances=1,
+            replace_existing=True
         )
 
         scheduler.add_job(
